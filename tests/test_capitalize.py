f3926d7465dbd91cfe79753eae40df68e9bc3a93
ArturIash <4ezyyy@gmail.com>
2022-12-01 00:58:33 +0300

    tests/test_capitalize.py

---
+++ tests/test_capitalize.py
@@ -0,0 +1,13 @@
+from capitalize import capitalize
+
+
+if capitalize('hello') != 'Hello':
+    raise Exception('works incorrectly')
+
+
+if capitalize('') != '':
+    raise Exception('works incorrectly')
+
+
+print('Test passed!')
+
